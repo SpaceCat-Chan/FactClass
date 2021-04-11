bcb3736920eb928268a7b7d84b78682604853e85
SpaceCat-Chan <49094338+SpaceCat-Chan@users.noreply.github.com>
2021-04-11 07:29:33 +0200

    completely new way to do things

--- class.lua
+++ class.lua
@@ -1,49 +1,49 @@
-local interface_table = {}
+local class_table = {}
+class_table.classes = {}
+class_table.inv_classes = {}
 
-local function copy_protected(child, parent)
-	for name, upper_parent in pairs(parent.meta.class.definition.inherit) do
-		copy_protected(child, upper_parent)
+function class_table.copy_parents(child, parent, parent_name)
+	for _,upper_parent in pairs(parent.meta.parents) do
+		class_table.copy_parents(child, upper_parent)
 	end
-	for protected_name, _ in pairs(parent.meta.class.definition.protected) do
-		child.meta.members.get[protected_name] = function(...)
-			return parent.meta.members.get[protected_name](...)
+	for variable, _ in pairs(parent.definition.protected) do
+		child.get[variable] = function(self)
+			if self.meta.self == self then
+				error("protected members can't be getted from public interface")
+			end
+			return self.meta.parents[parent_name].meta.members[variable]
 		end
-		child.meta.members.set[protected_name] = function(...)
-			return parent.meta.members.set[protected_name](...)
+		child.set[variable] = function(self, new)
+			if self.meta.self == self then
+				error("protected members can't be setted from public interface")
+			end
+			self.meta.parents[parent_name].meta.members[variable] = new
 		end
-		child.meta.members.call[protected_name] = function(...)
-			return parent.meta.members.call[protected_name](...)
+		child.call[variable] = function(self, ...)
+			if self.meta.self == self then
+				error("protected members cant be called from public interface")
+			end
+			parent.functions[variable](self.meta.parents[parent_name].meta.members, ...)
+		end
+	end
+	for variable, _ in pairs(parent.definition.public) do
+		child.get[variable] = function(self)
+			return self.meta.parents[parent_name][variable]
+		end
+		child.set[variable] = function(self, new)
+			self.meta.parents[parent_name][variable] = new
+		end
+		child.call[variable] = function(self, ...)
+			return parent.meta.functions[variable](self.meta.parents[parent_name].meta.members, ...)
 		end
 	end
 end
 
-local function copy_public(child, parent)
-	for _, upper_parent in pairs(parent.meta.class.definition.inherit) do
-		copy_public(child, upper_parent)
+function class_table.class(definition, name)
+	if name == nil then
+		error("remember class name")
 	end
-	for public_name, _ in pairs(parent.meta.class.definition.public) do
-		child.meta.members.get[public_name] = function(...)
-			return parent.meta.members.get[public_name](...)
-		end
-		child.meta.members.set[public_name] = function(...)
-			return parent.meta.members.set[public_name](...)
-		end
-		child.meta.members.call[public_name] = function(...)
-			return parent.meta.members.call[public_name](...)
-		end
-		child.get[public_name] = function(...)
-			return child.meta.members.get[public_name](...)
-		end
-		child.set[public_name] = function(...)
-			return child.meta.members.set[public_name](...)
-		end
-		child.call[public_name] = function(...)
-			return child.meta.members.call[public_name](...)
-		end
-	end
-end
 
-function interface_table.class(definition)
 	definition.inherit = definition.inherit or {}
 	definition.private = definition.private or {}
 	definition.protected = definition.protected or {}
@@ -52,88 +52,142 @@ function interface_table.class(definition)
 	local class = {}
 	class.definition = definition
 
-	function class:no_constructor_new()
+	class.get = {}
+	class.set = {}
+	class.call = {}
+	class.meta = {}
+	class.meta.functions = {}
+	class.meta.public_variables = {}
+	class.meta.other_variables = {}
+	class.meta.class = class
+	class.meta.name = name
+	class.meta.parents = {}
+
+	for parent_name, parent in pairs(definition.inherit) do
+		class.meta.parents[parent_name] = parent
+		class_table.copy_parents(class, parent, parent_name)
+	end
+
+	for variable, init in pairs(definition.private) do
+		class.get[variable] = function(self)
+			if self.meta.self == self then
+				error("private members can't be getted from public interface")
+			end
+			return self[variable]
+		end
+		class.set[variable] = function(self, new)
+			if self.meta.self == self then
+				error("private members can't be setted from public interface")
+			end
+			self[variable] = new
+		end
+		class.call[variable] = function(self, ...)
+			if self.meta.self == self then
+				error("private members cant be called from public interface")
+			end
+			return class.meta.functions[variable](self, ...)
+		end
+		if type(init) == "function" then
+			class.meta.functions[variable] = init
+		end
+	end
+
+	for variable, init in pairs(definition.protected) do
+		class.get[variable] = function(self)
+			if self.meta.self == self then
+				error("protected members can't be getted from public interface")
+			end
+			return self[variable]
+		end
+		class.set[variable] = function(self, new)
+			if self.meta.self == self then
+				error("protected members can't be setted from public interface")
+			end
+			self[variable] = new
+		end
+		class.call[variable] = function(self, ...)
+			if self.meta.self == self then
+				error("protected members cant be called from public interface")
+			end
+			return class.meta.functions[variable](self, ...)
+		end
+		if type(init) == "function" then
+			class.meta.functions[variable] = init
+		end
+	end
+
+	for variable, init in pairs(definition.public) do
+		class.get[variable] = function(self)
+			return self.meta.self[variable]
+		end
+		class.set[variable] = function(self, new)
+			self.meta.self[variable] = new
+		end
+		class.call[variable] = function(self, ...)
+			return class.meta.functions[variable](self.meta.members, ...)
+		end
+		if type(init) == "function" then
+			class.meta.functions[variable] = init
+		end
+	end
+
+	function class.new_no_construct()
 		local instance = {}
-		instance.get = {}
-		instance.set = {}
-		instance.call = {}
 		instance.meta = {}
-		instance.meta.self = instance
-		instance.meta.class = class
 		instance.meta.members = {}
-		instance.meta.members.get = {}
-		instance.meta.members.set = {}
-		instance.meta.members.call = {}
 		instance.meta.members.meta = instance.meta
-		instance.meta.actual_data = {}
 		instance.meta.parents = {}
+		instance.meta.class_name = name
+		instance.meta.self = instance
 
-		for name, parent in pairs(self.definition.inherit) do
-			instance.meta.parents[name] = parent:no_constructor_new()
-			copy_protected(instance, instance.meta.parents[name])
-			copy_public(instance, instance.meta.parents[name])
+		for parent_name, parent in pairs(class.definition.inherit) do
+			instance.meta.parents[parent_name] = parent:new_no_construct()
 		end
 
-		for private_name, private_value in pairs(self.definition.private) do
-			instance.meta.actual_data[private_name] = private_value
-			instance.meta.members.get[private_name] = function(...)
-				return instance.meta.actual_data[private_name]
-			end
-			instance.meta.members.set[private_name] = function(data)
-				instance.meta.actual_data[private_name] = data
-			end
-			instance.meta.members.call[private_name] = function(...)
-				return instance.meta.actual_data[private_name](instance.meta.members, ...)
+		for variable, init in pairs(definition.private) do
+			if type(init) ~= "function" then
+				instance.meta.members[variable] = init
 			end
 		end
 
-		for protected_name, protected_value in pairs(self.definition.protected) do
-			instance.meta.actual_data[protected_name] = protected_value
-			instance.meta.members.get[protected_name] = function(...)
-				return instance.meta.actual_data[protected_name]
-			end
-			instance.meta.members.set[protected_name] = function(data)
-				instance.meta.actual_data[protected_name] = data
-			end
-			instance.meta.members.call[protected_name] = function(...)
-				return instance.meta.actual_data[protected_name](instance.meta.members, ...)
+		for variable, init in pairs(definition.protected) do
+			if type(init) ~= "function" then
+				instance.meta.members[variable] = init
 			end
 		end
 
-		for public_name, public_value in pairs(self.definition.public) do
-			instance.meta.actual_data[public_name] = public_value
-			instance.meta.members.get[public_name] = function(...)
-				return instance.meta.actual_data[public_name]
-			end
-			instance.meta.members.set[public_name] = function(data)
-				instance.meta.actual_data[public_name] = data
-			end
-			instance.meta.members.call[public_name] = function(...)
-				return instance.meta.actual_data[public_name](instance.meta.members, ...)
-			end
-			instance.get[public_name] = function(...)
-				return instance.meta.members.get[public_name](...)
-			end
-			instance.set[public_name] = function(...)
-				return instance.meta.members.set[public_name](...)
-			end
-			instance.call[public_name] = function(...)
-				return instance.meta.members.call[public_name](...)
+		for variable, init in pairs(definition.public) do
+			if type(init) ~= "function" then
+				instance[variable] = init
 			end
 		end
 
 		return instance
 	end
 
-	function class:new(...)
-		local instance = self:no_constructor_new()
-		if instance.call.init then
-			instance.call.init(...)
+	function class.new(...)
+		local instance = class.new_no_construct()
+		if class.meta.functions.init then
+			class.meta.functions.init(instance.meta.members, ...)
 		end
 		return instance
 	end
 
+	class_table.classes[name] = class
+	class_table.inv_classes[class] = name
 	return class
 end
 
-return interface_table
+function class_table.get(self, name)
+	return class_table.classes[self.meta.class_name].get[name](self)
+end
+
+function class_table.set(self, name, data)
+	return class_table.classes[self.meta.class_name].set[name](self, data)
+end
+
+function class_table.call(self, name, ...)
+	return class_table.classes[self.meta.class_name].call[name](self, ...)
+end
+
+return class_table
--- tests/basic/main.lua
+++ tests/basic/main.lua
@@ -9,20 +9,20 @@ local Animal = {
     },
     public = {
         get_leg_number = function(self)
-            return self.get.leg_number(self)
+            return Class.get(self, "leg_number")
         end,
         get_speaks = function(self)
-            return self.get.speaks(self)
+            return Class.get(self, "speaks")
         end
     }
 }
-Animal = Class.class(Animal)
+Animal = Class.class(Animal, "Animal")
 
-local my_animal = Animal:new()
+local my_animal = Animal.new()
 
 print("Generic Animal:")
-print(my_animal.call.get_leg_number())
-print(my_animal.call.get_speaks())
+print(Animal.call.get_leg_number(my_animal))
+print(Animal.call.get_speaks(my_animal))
 
 local Human = {
     inherit = {
@@ -30,18 +30,18 @@ local Human = {
     },
     public = {
         init = function(self)
-            self.set.leg_number(2)
-            self.set.speaks(true)
+            Class.set(self, "leg_number", 2)
+            Class.set(self, "speaks", true)
         end
     }
 }
-Human = Class.class(Human)
+Human = Class.class(Human, "Human")
 
-local my_human = Human:new()
+local my_human = Human.new()
 
 print("\nHuman:")
-print(my_human.call.get_leg_number())
-print(my_human.call.get_speaks())
+print(Human.call.get_leg_number(my_human))
+print(Human.call.get_speaks(my_human))
 
 local many_leg = {
     inherit = {
@@ -49,27 +49,35 @@ local many_leg = {
     },
     public = {
         set_leg_number = function(self, leg_count)
-            self.set.leg_number(leg_count)
+            Class.set(self, "leg_number", leg_count)
         end,
         init = function(self, leg_count)
-            self.set.leg_number(leg_count)
+            Class.set(self, "leg_number", leg_count)
         end
     }
 }
-many_leg = Class.class(many_leg)
+many_leg = Class.class(many_leg, "many_leg")
 
-local leg_10 = many_leg:new(10)
-local leg_20 = many_leg:new(20)
+local leg_10 = many_leg.new(10)
+local leg_20 = many_leg.new(20)
 
 print("\nleg 10:")
-print(leg_10.call.get_leg_number())
-print(leg_10.call.get_speaks())
+print(many_leg.call.get_leg_number(leg_10))
+print(many_leg.call.get_speaks(leg_10))
 
 print("\nleg 20:")
-print(leg_20.call.get_leg_number())
-print(leg_20.call.get_speaks())
+print(many_leg.call.get_leg_number(leg_20))
+print(many_leg.call.get_speaks(leg_20))
 
-leg_10.call.set_leg_number(4)
+many_leg.call.set_leg_number(leg_10, 4)
 print("\nleg 10 new:")
-print(leg_10.call.get_leg_number())
-print(leg_10.call.get_speaks())
+print(many_leg.call.get_leg_number(leg_10))
+print(many_leg.call.get_speaks(leg_10))
+
+
+local many_things = {Animal.new(), Human.new(), Animal.new(), many_leg.new(42)}
+for i, v in pairs(many_things) do
+    print("\narray index "..tostring(i)..":")
+    print(Class.call(v, "get_leg_number"))
+    print(Class.call(v, "get_speaks"))
+end
